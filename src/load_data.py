842abf14f7bc34ae959dede5314da749a9a2f138
cwtravisyip <cwtravisyip@gmail.com>
2025-09-22 22:09:19 +0100

    Remove redundant load_zip_file function

--- src/load_data.py
+++ src/load_data.py
@@ -5,22 +5,6 @@ import pandas as pd
 from typing import Generator, Union, Optional
 import warnings
 
-
-def load_zip_file(url:str,path: str = 'unzipped_files') -> None:
-    """
-    Unzip zip files from url and store in extract to specified directory.
-    """
-    response = requests.get(url)
-    response.raise_for_status() 
-
-    with zipfile.ZipFile(io.BytesIO(response.content)) as zip_file:
-            
-        # Optionally extract files to a folder
-        zip_file.extractall(path)
-        print(f"Files extracted to {path} folder.")
-
-    return None
-
 def find_csv(res: requests.models.Response) -> list:
     """
     Open the zipfile returned from a request response and return the filenames in a list
@@ -36,8 +20,7 @@ def find_csv(res: requests.models.Response) -> list:
             warnings.warn(f"There exists {len(csv_files)} csv file in the zipfile.")
     
     return csv_files
-    
-    
+
 def load_csv_in_zip(url:str, chunksize: Optional[int] = 5000) -> Union[pd.DataFrame, Generator[pd.DataFrame, None, None]]:
         """
         Unzip the zipfile returned from a requests response and open the first csv file in the filename list passed in.
